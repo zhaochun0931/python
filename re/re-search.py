bd677bc6c08582cae2c9e07a0c97b1199ea41801
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-07-06 21:48:26 +0800

    Update re-search.py

--- re/re-search.py
+++ re/re-search.py
@@ -15,3 +15,26 @@ print("end index:",xx.end())
 
 # start index: 6
 # end index: 10
+
+
+
+
+
+
+
+
+xx = re.match('hello',txt)
+print(xx)
+
+# <re.Match object; span=(0, 5), match='hello'>
+
+xx =  re.match('this',txt)
+print(xx)
+
+# None
+
+
+xx = re.search('this',txt)
+print(xx)
+
+# <re.Match object; span=(6, 10), match='this'>
