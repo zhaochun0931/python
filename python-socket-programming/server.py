8b272274b56857fb0cfc25a5a57cb29e740b90cd
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-03-01 17:22:30 +0800

    Update server.py

--- python-socket-programming/server.py
+++ python-socket-programming/server.py
@@ -3,7 +3,7 @@ import time
 
 xx = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-# it will cause the remote server cannot connect to this host with the port 8080
+# it will cause the remote server cannot connect to this host with the port 8080, or you can connect from the local host
 # host = '127.0.0.1'
 
 # listening on any interface
