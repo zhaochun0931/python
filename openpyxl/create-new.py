7c8e637b14698c3df3fc192984c8dcb911af2286
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-01-14 18:19:03 +0800

    Update create-new.py

--- openpyxl/create-new.py
+++ openpyxl/create-new.py
@@ -3,7 +3,7 @@ import openpyxl
 # Create a new workbook
 wb = openpyxl.Workbook()
 
-# Select the active sheet, By default, openpyxl creates a workbook with one sheet.
+# Select the active sheet
 sheet = wb.active
 
 # Insert a value into a specific cell
@@ -11,6 +11,10 @@ sheet['A1'] = "Hello, World!"  # Insert value into cell A1
 
 # Optionally, you can insert data into more cells:
 sheet['B1'] = "This is a test"
+
+sheet.cell(10,8).value = 400
+
+sheet.cell(row=3, column=3).value = 300
 
 # Save the workbook to a file
 wb.save("new_excel_file.xlsx")
