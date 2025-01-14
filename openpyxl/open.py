1243c87fed072a6b1c13a33e9eade00309999d9f
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-01-14 19:14:16 +0800

    Update open.py

--- openpyxl/open.py
+++ openpyxl/open.py
@@ -11,3 +11,8 @@ sheet_names = wb.sheetnames
 # Print the sheet names and count
 print(f"The workbook contains {len(sheet_names)} sheet(s).")
 print("Sheet names:", sheet_names)
+
+for i in range(len(sheet_names)):
+    sheet_temp_name = sheet_names[i]
+    sheet = wb[sheet_temp_name]
+    print(sheet_names[i], sheet.max_column, sheet.max_row)
