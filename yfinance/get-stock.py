f33a05098e746b5c04521470d43f00fa29403027
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-01-27 20:11:58 +0800

    Update and rename stock.py to get-stock.py

---
+++ yfinance/get-stock.py
@@ -0,0 +1,23 @@
+import yfinance as yf
+import pandas as pd
+import mplfinance as mpf
+import matplotlib.pyplot as plt
+
+
+
+
+# 获取股票数据
+stock_symbol = '002956.sz'
+data = yf.download(stock_symbol, start='2020-01-01', end='2025-01-27')
+
+pd.set_option('display.float_format', '{:.2f}'.format)
+
+
+# 打印数据框
+print(data)
+
+
+data.to_excel(f'{stock_symbol}_stock_data.xlsx', index=True)
+
+
+#data.to_excel('data.xlsx', index=False)
