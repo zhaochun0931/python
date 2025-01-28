44de2b8208a832ebbe879be0ff80f4800a0886ed
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-01-28 15:32:12 +0800

    Update pandas-dataframe.py

--- pandas/pandas-dataframe.py
+++ pandas/pandas-dataframe.py
@@ -1,5 +1,7 @@
 # A Pandas DataFrame is a 2 dimensional data structure, like a 2 dimensional array, or a table with rows and columns.
 
+import pandas as pd
+
 
 data = {
   "calories": [420, 380, 390],
